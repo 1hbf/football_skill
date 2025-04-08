55f5fbe0455014a9ad0ac1419b64f48f372bce69
Hisham Bassam <abo3tb0092@gmail.com>
2025-04-08 07:24:56 +0300

    main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://v0-score-tech.vercel.app/performance-analysis"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
